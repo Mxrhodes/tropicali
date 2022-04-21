c8a28ae481ee6687a5990352ed8edaacc64d7319
Mateo Rhodes <rhodesmateo@gmail.com>
2022-04-21 13:15:59 -0400

    site looks ready to go

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,29 @@
 var gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
 
+// css
 let cleanCss = require("gulp-clean-css");
+let postcss = require("gulp-postcss");
 let sourcemaps = require("gulp-sourcemaps");
+let concat = require("gulp-concat");
+
+// browser refresh
 let browserSync = require("browser-sync").create();
+
+// images
 let imagemin = require('gulp-imagemin');
 
+// github
 let ghpages = require('gh-pages');
 
-sass.compiler = require('sass');
 
-gulp.task('sass', function() {
-    // Run "sass css/app.scss app.css --watch"
-    return gulp.src("src/css/app.scss") // Get this file
+gulp.task('css', function() {
+    return gulp.src(
+        ['src/css/reset.css', 
+        'src/css/typography.css', 
+        'src/css/app.css']) // Get this file
         .pipe(sourcemaps.init())
-            .pipe(sass()) // Run sass on the file
+        .pipe(postcss([ require('autoprefixer'), require('postcss-preset-env')({stage: 1, browsers:["IE 11", "last 2 versions"]}) ]))
+        .pipe(concat("app.css"))
             .pipe(cleanCss({compatibility: 'ie8'})) // Add CleanCSS to the file
         .pipe(sourcemaps.write()) // This will allow Dev tools to give accurate location for code correction
         .pipe(gulp.dest("dist")) // Send the clean file to the dist folder
@@ -50,7 +59,7 @@ gulp.task('watch', async function () {
 
 
     gulp.watch("src/*.html", gulp.series('html')).on('change', browserSync.reload)
-    gulp.watch("src/css/app.scss", gulp.series('sass'))
+    gulp.watch("src/css/*", gulp.series('css'))
     gulp.watch("src/fonts/*", gulp.series('fonts'))
     gulp.watch("src/img/*", gulp.series('images'))
 });
@@ -59,4 +68,4 @@ gulp.task('deploy', async function() {
     ghpages.publish('dist')
 });
 
-gulp.task('default', gulp.series('html', 'sass', 'fonts', 'images', 'watch'));
+gulp.task('default', gulp.series('html', 'css', 'fonts', 'images', 'watch'));
